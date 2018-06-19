6dc9580fd60c24abbee6567960c01d6b6f6653fe
Santiago <sgobotta@gmail.com>
2018-06-18 23:31:46 -0300

    Implements a Map component where the Chart is rendered

---
+++ app/scripts/components/Maps.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Chart } from 'react-google-charts';
+
+export default class App extends React.Component {
+  static propTypes = {
+    votes: PropTypes.array.isRequired,
+  };
+
+  render() {
+    const { votes } = this.props;
+    const options = {
+      region: 'AR',
+      resolution: 'provinces',
+      colorAxis: {
+        minValue: 0,
+        maxValue: 100,
+        colors: ['#FF0000', '#00FF00'],
+      },
+    };
+    return (
+      <div className="my-pretty-chart-container">
+        <Chart
+          chartType="GeoChart"
+          width="100%"
+          height="900px"
+          data={votes}
+          graph_id="GeoChart"
+          options={options}
+        />
+      </div>
+    );
+  }
+}
