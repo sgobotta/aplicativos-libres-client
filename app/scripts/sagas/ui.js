d4aa2c5b42b3fcef17a9e874905ea574f1e8c7c5
Santiago <sgobotta@gmail.com>
2018-08-07 18:51:25 -0300

    Creates the UI tab swapping sagas

--- app/scripts/sagas/ui.js
+++ app/scripts/sagas/ui.js
@@ -18,7 +18,7 @@ export function* toggleDrawer({ payload }) {
     if (!payload.redirectsTo) return;
     yield call(payload.redirectsTo);
     yield put({
-      type: ActionTypes.TOGGLE_DRAWER_FULLFILLED,
+      type: ActionTypes.TOGGLE_DRAWER_FULFILLED,
     });
   }
   catch (err) {
@@ -30,11 +30,27 @@ export function* toggleDrawer({ payload }) {
   }
 }
 
+export function* goToOrderTab({ payload }) {
+  try {
+    yield put({
+      type: ActionTypes.SWAP_ORDERS_TABS_FULFILLED,
+      payload,
+    });
+  }
+  catch (err) {
+    yield put({
+      type: ActionTypes.SWAP_ORDERS_TABS_FAILURE,
+      payload: err,
+    });
+  }
+}
+
 /**
  * UI Sagas
  */
 export default function* root() {
   yield all([
     takeLatest(ActionTypes.TOGGLE_DRAWER, toggleDrawer),
+    takeLatest(ActionTypes.SWAP_ORDERS_TABS, goToOrderTab),
   ]);
 }
