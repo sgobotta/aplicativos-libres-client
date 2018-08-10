16b4919f9dcba5a51bf14863235c6f3cd3d9d0b4
Santiago <sgobotta@gmail.com>
2018-08-10 00:34:14 -0300

    Added style and link to footer

--- app/scripts/components/Footer.jsx
+++ app/scripts/components/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 const Footer = () => (
   <footer className="app__footer">
     <div className="app__container">
-      <h6>sgobotta</h6>
+      <Typography align="left" variant="title" color="inherit">
+        <a href="https://github.com/sgobotta/" target="_blank">
+          sgobotta
+        </a>
+      </Typography>
     </div>
   </footer>
 );
