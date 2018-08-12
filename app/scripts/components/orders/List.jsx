997f3dfc3bbc6c1be7cb840814036e4a48381482
Santiago <sgobotta@gmail.com>
2018-08-12 18:15:08 -0300

    Adds order visualization, buttons and date rendering

--- app/scripts/components/orders/List.jsx
+++ app/scripts/components/orders/List.jsx
@@ -8,17 +8,31 @@ import { findOrders } from 'actions';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-// import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-
+import DeleteIcon from '@material-ui/icons/Delete';
+/** Miscellaneous Imports */
+import { DateUtils } from 'utils';
 
 const styles = {
-  card: {
+  mainCard: {
     minWidth: '100%',
     borderRadius: '3px',
+  },
+  itemCard: {
+    padding: '12px 12px 12px 12px',
+    borderRadius: '0px',
+  },
+  cardIcon: {
+    color: 'red',
+  },
+  username: {
+    fontWeight: 'bold',
+  },
+  creationDate: {
+    fontStyle: 'italic',
   },
 };
 
@@ -34,13 +48,67 @@ class OrderList extends React.Component {
     dispatch(findOrders());
   }
 
-  renderOrders(classes, orders) {
+
+  /**
+   * Render Methods
+   * */
+
+  renderUsername(author) {
+    const { user, classes } = this.props;
+    if (author.id === user.data.id) {
+      return (
+        <span className={classes.username}>Yo</span>
+      );
+    }
+    return author.username;
+  }
+
+  renderCreationDate(creationDate) {
+    const { classes } = this.props;
+    const date = DateUtils.getElapsedTime(creationDate);
+    return (
+      <span className={classes.creationDate}>{date}</span>
+    );
+  }
+
+  renderDeleteAction(authorId) {
+    const { classes, user } = this.props;
+    if (authorId === user.data.id) {
+      return (
+        <Button
+          size="small"
+          onClick={this.handleClickLogin}
+        >
+          <DeleteIcon className={classes.cardIcon} />
+        </Button>
+      );
+    }
+    return null;
+  }
+
+  renderOrders() {
+    const { classes, orders } = this.props;
     if (orders && orders.queryResult && orders.queryResult.data && orders.isFinished) {
       const output = orders.queryResult.data.map((order) => (
-        <Card key={order._id} className={classes.card}>
-          <Typography variant="body2">
-            order.author
-          </Typography>
+        <Card key={order.id} className={classes.itemCard}>
+          <Grid container direction="row">
+            <Grid item xs={11} md={10}>
+              <Typography variant="title">
+                {order.title}
+              </Typography>
+              <Typography variant="body2">
+                {this.renderUsername(order.author)}
+              </Typography>
+            </Grid>
+            <Grid item xs={1} md={2} align="right">
+              { this.renderDeleteAction(order.author.id) }
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="body2" align="right">
+                {this.renderCreationDate(order.creationDate)}
+              </Typography>
+            </Grid>
+          </Grid>
         </Card>
       ));
       return output;
@@ -49,15 +117,12 @@ class OrderList extends React.Component {
   }
 
   render() {
-    const { classes, orders } = this.props;
+    const { classes } = this.props;
     return (
-      <Grid item xs={12}>
-        <Card className={classes.card}>
+      <Grid>
+        <Card className={classes.mainCard}>
           <CardContent>
-            <Typography variant="title">
-              Pedidos
-            </Typography>
-            { this.renderOrders(orders) }
+            { this.renderOrders() }
           </CardContent>
           <CardActions>
             <Button
@@ -79,12 +144,11 @@ OrderList.propTypes = {
   classes: PropTypes.object.isRequired,
   dispatch: PropTypes.func.isRequired,
   orders: PropTypes.object.isRequired,
-  ui: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
 };
 
-/* istanbul ignore next */
 function mapStateToProps(state) {
-  return { orders: state.orders, ui: state.ui };
+  return { orders: state.orders, ui: state.ui, user: state.user };
 }
 
 export default connect(mapStateToProps)(withStyles(styles)(OrderList));
