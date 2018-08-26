f7a4ea4a8f2471da3df6cb3107539edf7f3b367d
Santiago <sgobotta@gmail.com>
2018-08-26 01:08:49 -0300

    Drawer: adds styles and statistics link

--- app/scripts/components/Drawer.jsx
+++ app/scripts/components/Drawer.jsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 /** Redux Imports */
-import { toggleDrawer } from 'actions';
+import { toggleDrawer, logOut } from 'actions';
 /** Material UI Imports */
 import classNames from 'classnames';
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles, createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -16,15 +16,31 @@ import IconButton from '@material-ui/core/IconButton';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import SettingsIcon from '@material-ui/icons/SettingsSharp';
 import Restaurant from '@material-ui/icons/Restaurant';
+import PowerSettingsNew from '@material-ui/icons/PowerSettingsNew';
+import Assessment from '@material-ui/icons/Assessment';
 
+
+const drawerTheme = createMuiTheme({
+  overrides: {
+    MuiListItem: {
+      root: {
+        paddingTop: '0px',
+        paddingBottom: '0px',
+      },
+    },
+    MuiPaper: {
+      root: {
+        backgroundColor: 'rgb(180, 205, 231)',
+        borderTopRightRadius: '8px',
+        borderBottomRightRadius: '8px',
+        border: '1px solid rgb(0, 120, 170)',
+        borderLeft: '0px',
+      },
+    },
+  },
+});
 
 const styles = {
-  list: {
-    width: 250,
-  },
-  fullList: {
-    width: 'auto',
-  },
   toolbar: {
     display: 'flex',
     alignItems: 'center',
@@ -47,6 +63,10 @@ class SwipeableTemporaryDrawer extends React.Component {
     });
   }
 
+  handleClickLogout = () => {
+    this.props.dispatch(logOut());
+  };
+
   toggleDrawer = () => () => {
     this.props.dispatch(toggleDrawer());
   };
@@ -55,37 +75,75 @@ class SwipeableTemporaryDrawer extends React.Component {
     this.props.dispatch(toggleDrawer({ redirectsTo: () => this.props.history.push(path) }));
   }
 
-  renderOrders() {
+  renderStatistics() {
     return (
-      <ListItem button onClick={this.goToRoute('/orders')}>
+      <ListItem button onClick={this.goToRoute('/map')}>
         <ListItemIcon>
           <IconButton>
-            <Restaurant />
+            <Assessment />
           </IconButton>
         </ListItemIcon>
-        <ListItemText primary="Pedidos" />
+        <ListItemText primary="Estadísticas" />
       </ListItem>
     );
   }
 
+  renderOrders() {
+    const { user } = this.props;
+    if (user.isAuthenticated) {
+      return (
+        <ListItem button onClick={this.goToRoute('/orders')}>
+          <ListItemIcon>
+            <IconButton>
+              <Restaurant />
+            </IconButton>
+          </ListItemIcon>
+          <ListItemText primary="Pedidos" />
+        </ListItem>
+      );
+    }
+    return null;
+  }
+
   renderSettings() {
-    return (
-      <ListItem button onClick={this.goToRoute('/settings')}>
-        <ListItemIcon>
-          <IconButton>
-            <SettingsIcon />
-          </IconButton>
-        </ListItemIcon>
-        <ListItemText primary="Configuraciones" />
-      </ListItem>
-    );
+    const { user } = this.props;
+    if (user.isAuthenticated) {
+      return (
+        <ListItem button onClick={this.goToRoute('/settings')}>
+          <ListItemIcon>
+            <IconButton>
+              <SettingsIcon />
+            </IconButton>
+          </ListItemIcon>
+          <ListItemText primary="Configuraciones" />
+        </ListItem>
+      );
+    }
+    return null;
+  }
+
+  renderLogout() {
+    const { user } = this.props;
+    if (user.isAuthenticated) {
+      return (
+        <ListItem button onClick={this.handleClickLogout}>
+          <ListItemIcon>
+            <IconButton>
+              <PowerSettingsNew />
+            </IconButton>
+          </ListItemIcon>
+          <ListItemText primary="Cerrar Sesión" />
+        </ListItem>
+      );
+    }
+    return null;
   }
 
   render() {
     const { classes } = this.props;
 
     return (
-      <div>
+      <MuiThemeProvider theme={drawerTheme}>
         <SwipeableDrawer
           classes={{ paper: classNames(classes.drawerPaper) }}
           anchor="left"
@@ -99,14 +157,16 @@ class SwipeableTemporaryDrawer extends React.Component {
             </IconButton>
           </div>
           <Divider />
-          <List>
-            { this.renderOrders() }
+          <List disablePadding={true}>
+            { this.renderStatistics() }
             <Divider />
+            { this.renderOrders() }
             { this.renderSettings() }
+            { this.renderLogout() }
           </List>
           <Divider />
         </SwipeableDrawer>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
@@ -116,6 +176,7 @@ SwipeableTemporaryDrawer.propTypes = {
   dispatch: PropTypes.func.isRequired,
   drawer: PropTypes.object.isRequired,
   history: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(SwipeableTemporaryDrawer);
