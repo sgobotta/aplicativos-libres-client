440e8ee58b19be3bd5dc9c7cd68b4ad2b2d715b4
Santiago <sgobotta@gmail.com>
2018-08-26 03:33:49 -0300

    Footer: styling
    + Adds github image

--- app/scripts/components/Footer.jsx
+++ app/scripts/components/Footer.jsx
@@ -4,8 +4,13 @@ import Typography from '@material-ui/core/Typography';
 const Footer = () => (
   <footer className="app__footer">
     <div className="app__container">
-      <Typography align="left" variant="title" color="inherit">
+      <Typography align="left" variant="title" color="secondary">
         <a href="https://github.com/sgobotta/" target="_blank">
+          <img
+            style={{ width: '30px', height: '30px' }}
+            src={require('assets/media/images/github_mark.png')}
+            alt="Github"
+          />
           sgobotta
         </a>
       </Typography>
