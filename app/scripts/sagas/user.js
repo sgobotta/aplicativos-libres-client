f71afd386a12db0bd7722aefedb8f9d5e722d39a
Santiago <sgobotta@gmail.com>
2018-08-26 23:38:06 -0300

    Users Service: implements the saga with patch and onPatched services

--- app/scripts/sagas/user.js
+++ app/scripts/sagas/user.js
@@ -6,12 +6,13 @@
 import { delay } from 'redux-saga';
 import { all, call, put, takeLatest } from 'redux-saga/effects';
 import { ActionTypes } from 'constants/index';
+import { showAlert } from 'actions';
 import { showLoading, hideLoading } from 'react-redux-loading-bar';
-
 import { request } from './../modules/socket-client';
+import { messages } from './../utils/notifications/alert-messages';
 
 /**
- * Login
+ * @function login
  */
 export function* login({ payload }) {
   try {
@@ -41,7 +42,7 @@ export function* login({ payload }) {
 }
 
 /**
- * Logout
+ * @function logout
  */
 export function* logout() {
   try {
@@ -61,11 +62,58 @@ export function* logout() {
 }
 
 /**
+ * @function patchUser
+ */
+export function* patchUser({ payload }) {
+  const alertNotification = messages.services.users[payload.data.service];
+  const { dispatch } = payload.data;
+  try {
+    const service = {
+      service: 'users',
+      action: 'patch',
+      payload,
+    };
+    const response = yield call(request, service);
+    const patchService = {
+      service: 'users',
+      action: 'onPatched',
+      payload: response,
+      dispatch,
+    };
+    yield call(request, patchService);
+    yield put({
+      type: ActionTypes.SERVICES_USERS_PATCH_FULFILLED,
+      payload: response,
+    });
+    dispatch(
+      showAlert(
+        alertNotification.message,
+        { type: alertNotification.type, icon: alertNotification.icon }
+      )
+    );
+  }
+  catch (err) {
+    yield put({
+      type: ActionTypes.SERVICES_USERS_PATCH_REJECTED,
+      payload: err,
+    });
+    dispatch(
+      showAlert(
+        'Algo salió mal al actualizar tus configuraciones',
+        { type: 'error', icon: 'i-bell' }
+      )
+    );
+  }
+}
+
+
+/**
  * User Sagas
  */
 export default function* root() {
   yield all([
     takeLatest(ActionTypes.USER_LOGIN_REQUEST, login),
     takeLatest(ActionTypes.USER_LOGOUT_REQUEST, logout),
+    takeLatest(ActionTypes.SERVICES_USERS_PATCH, patchUser),
   ]);
 }
