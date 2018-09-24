552bffff498bcc86ef68f3ab047fd29f60fe573e
Santiago <sgobotta@gmail.com>
2018-09-24 01:09:46 -0300

    Adds temp fake data for tests

---
+++ app/scripts/components/events/allFakeData.js
@@ -0,0 +1,225 @@
+const username = 'santiago';
+
+
+const titleJazz = 'Martes de Jazz y Música Popular ✰ Oscar Giunta Supertrío!';
+const descriptionJazz = `· EDICIÓN DELUXE DEL MARTES DE JAZZ Y MÚSICA POPULAR, Ciclo producido por Tempuja / Sello de Arte todos los Martes en Blend Pub.
+
+Presentamos mega felices a
+► Oscar Giunta SUPERTRÍO!
+Oscar Giunta en batería, composición, arreglos y dirección musical, Hernan Jacinto en piano y Flavio Romero en contrabajo.
+Ya nos visitaron este año, estuvo INCREÍBLE! Así que volvemos a repetir ♥
+Músicos de primer nivel ♦
+
+Les invitamos a su mesa cerca del espectáculo llamando a Blend Pub al : 4253 6058 de 18 a 20 Hs. Con reserva de mesa se cobrará un derecho de show de $100 por persona sobre el valor de lo consumido. Ingresando sin reserva se cobrará en puerta $200 y contarán con una consumición.
+
+:::::::::::::::::::::::::50% OFF en TODAS las Cervezas Artesanales Tiradas (18hs a 21hs):::::::::::::::::::::::
+
+Lxs esperamos! ㋡
+
+Musicalizador oficial de cada Martes: Marcelo Calona
+
+Produce: Tempuja / Sello de Arte 〄 ⓤⓝ ⓟⓡⓞⓓⓤⓒⓣⓞ ⓓⓔ ⓣⓤ ⓜⓔⓝⓣⓔ`;
+
+
+const titleDiaphragm = 'Ciclo Diaphragm ∆ Vol. XXXII Vulebard + VirtualFrizz ·';
+const descriptionDiaphragm = `¡¡Bienvenidxs Diaphragmers a una nueva edición!!
+¡Después de un breve receso volvemos a pleno y con #buenasnuevas!
+¡Y como estamos de Festejo! ¡Te proponemos este increíble Line Up! 🔥
+△ Bandas en vivo:
+Vulebard (#hardrock integrado por Guido Vulebard en Bajo y voz, Hernan Vicente en Batería, Tomi Olmos en Guitarra, Seba Gimenez en Guitarra y Ale Bougie en Teclado.)
+Virtual Frizz (#PowerTrio integrado por Lulo García en Guitarra y voz, Algo De Luca en Batería y Andrés Díaz Santana en Bajo.)
+△ Expo Collage: Diamonsi (Marita Bossi)
+△ Flash Tattoo: Carolina Lio #bestfriend de la casa
+△ Diaphragm Dj Set!
+
+👉 Y además tenemos Nueva Locación → ¡Agradecemos a Biergarten Bernal por abrirnos sus puertas y te contamos que podes venir a disfrutar de unas excelentes artesanales y más; además del exquisite Menú que tienen para vos!
+
+
+Como siempre todas las regulaciones y sonidos a cargo del Diaphragm team (John Braian Lora, Romi Mario y Marcelo Ramirez)
+Organizan: Ciclo Diaphragm y
+Tempuja / Sello de Arte ⓤⓝ ⓟⓡⓞⓓⓤⓒⓣⓞ ⓓⓔ ⓣⓤ ⓜⓔⓝⓣⓔ
+
+Diseño Flyer: Pablo Julián (Chamax)
+Ph: Ezequiel Iglesias`
+
+
+const titleAndamio = 'El pintor a la luna y Les Minots en el Andamio - Quilmes';
+const descriptionAndamio = `Ciertas constelaciones se pusieron de acuerdo para que este encuentro suceda en la bella y agitada ciudad de Quilmes, el 25 de Agosto. Todos al El Andamio Espacio Cultural a volar con El pintor a la luna y Les Minots.
+Hipolito Yrigoyen 58 - Quilmes - 21hs
+Entrada al sobre.`;
+
+
+const titleUNQuilombo = 'Unquilombo - Fiesta de la Primavera - PEM & Siberia';
+const descriptionUNQuilombo = `FIESTA DE LA PRIMAVERA EN EL LOCAL DE AL LADO DE LA #UNQ !
+
+QUE PASA??
+
+TOCAN ALTAS BANDAS:
+- Paul Está Muerto
+- Siberia Canaria
+
+#DJ SETS
+
+#FIESTA & #BAILE
+
+#BARRA MEGA BARATA
+
+LA MOVIDA DE LA #UNQ EN UNA SOLA FIESTA PRIMAVERAL !
+
+
+#ENTRADA gratis
+
+Todo lo recaudado es la para creación del #LAV
+
+Diseño de flyer: Feche Bernet`;
+
+
+const titleViernesDeSamba = 'Viernes de Samba! Día de la Primavera junto a Cafundó';
+const descriptionViernesDeSamba = `VIERNES DE SAMBA!
+**Un portal a Río de Janeiro en la noche porteña**
+Se viene una noche especial... Viernes de Primavera junto a los amigos de CAFUNDÓ, te esperamos en este tremendo fiestón.
+Como siempre el Show de la Escola de Samba Estação Primeira de Lanús y toda la Comunidad de Primeira festejando los #10AÑOSE1 y "batendo na palma da mão" con Roda de Samba Bom Ambiente.
+Todo esto en el palacio del samba, el Chau Che Clú.
+Parrilla y barra!
+Apertura 21hs. / Capacidad Limitada.
+Entrada al club $150.-
+Te esperamos en Av. Vélez Sársfield 222 - Barracas, Capital`;
+
+
+const titleAuditorioKraft = 'La Máquina De Hacer Pajaros (álbum) en Parlantes Holofónicos';
+const descriptionAuditorioKraft = `Lunes 24 de Septiembre | 21:00hs tendremos el ábum "LA MÁQUINA DE HACER PAJAROS" de la banda liderada por Charly Gracía: LA MÁQUINA DE HACER PAJAROS, a través de los PARLANTES HOLOFONICOS.
+
+◎ La cita será en Auditorio Kraft - Florida 681 2do Subsuelo, C.A.B.A.
+
+◎ La reproducción de la obra será realizada en total oscuridad.
+
+◎ Previo a la reproducción de la obra habrá una breve explicación técnica sobre ciertos aspectos del sonido y de la tecnología de los Parlantes Holofónicos.
+
+►Esta tecnología reproduce la naturaleza tímbrica del sonido de una manera nunca antes igualada, y gracias a la oscuridad podrás liberarte del entorno para disfrutar de la música plenamente.
+
+►► Los mejores parlantes del mundo, únicamente en la Ciudad de Buenos Aires.
+
+►►► Una experiencia sonora, para todos los sentidos.
+
+Adquirí tus entradas a través de Passline y boleterías de Auditorio Kraft | Horarios de la boletería: Lunes de 19 a 22:00hs - Miércoles de 19 a 22hs - Viernes de 19 a 23hs |.
+
+Tickets anticipados en el link ⤵
+https://www.passline.com/sitio-evento/la-mquina-de-hacer-pajaros--la-mquina-de-hacer-pajaros--parlantes-holofonicos
+
+• Anticipadas: $200
+• Valor en puerta (Mismo día / Al momento del evento): $250
+(*)Los precios son por función / horario individual.
+
+El 2x1 es por cupo limitado y podés adquirirlo a través de Passline.
+
+El evento es ATP.
+
+Auditorio Kraft: Florida 681 2do Subsuelo, C.A.B.A.
+
+¡Los esperamos para disfrutar de sus artistas favoritos como nunca!`;
+
+
+const titleInterlunio = 'Gabo Ferro en el bosque Interlunar';
+const descriptionInterlunio = `Bono contrib. Semillas de menta - Libros elegidos (Garibaldi 215, Quilmes) horarios: Lun-mie 15.30 a 20//Jue-Sab 10 13.30 // 15.30 a 20. Domingo cerrado
+
+Mientras afuera el plástico lo va envolviendo todo y el entretenimiento va empaquetando todo en unos y ceros. Hay algo que escapa a esa traducción. Hay un presente que no es fuga, sino que permanece como memoria del cuerpo. Una sopa con ingredientes de poesía, de voz. Una revancha de lo ordinario, "que se ha vuelto invisible y que renueva su función… siempre mágica, siempre – al fin – extraordinaria".
+
+Será una vez más, entre árboles, arbustos, pasto y la maleza (claro que sí) que habitan en el bosque Interlunar... junto a la luna y las estrellas donde aún resuena la belleza que nos convidó Gabo Ferro en 2017 y que cada vez se vuelve más imprescindible...
+
+Cantante, autor y compositor. Poeta. Performer. Historiador.
+Desde la edición de su primer disco solista en el año 2005 es considerado por el público y la crítica como uno de los mejores cantantes y compositores en su género. Ha dado conciertos y recitales en Argentina, América y Europa. Con su sello Costurera carpintero ha producido de manera independiente catorce discos: ocho de estudio como solista, tres en colaboración con otros artistas y tres “piratas”.
+
+La primera noche del fantasma (2013) obtuvo el Premio Carlos Gardel en la categoría Mejor Álbum Canción Testimonial o de Autor; una de sus canciones “Volver a Volver” fue incluida con mucha dedicación en los programas de televisión Farsantes (2013) y La Leona (2016), en este último programa varias canciones de Gabo acompañaron escenas difícilmente olvidables. El veneno de los milagros (2014) -con canciones de su autoría grabadas junto con Luciana Jury- fue nominado como Álbum del Año (Gardel de Oro) y Mejor Álbum Canción Testimonial o de Autor para los Premios Carlos Gardel 2015. Una de las canciones de este disco fue incluida en la película ZONDA de Carlos Saura. Ese mismo año Gabo fue reconocido con el Premio Konex como una de las cinco mejores figuras de la década 2005-2015 en la disciplina Canción de Autor.
+
+En 2014 La Marca Editora publica Costurera carpintero, una antología de las letras de las canciones de todos sus discos, en el prólogo Diana Bellessi afirma: “La poesía de Gabo Ferro es la poesía de un mago. Alguien que puede hacer de las palabras siempre algo imprevisto. Hablar del mal y convertirlo en bien, hablar del bien y convertirlo en dolor, hablar de la muerte y transformarla en sembradora, en dadora de vida. Por eso, en sus poemas suceden cosas extraordinarias”. En 2015 edita su primer poemario Recetario panorámico elemental fantástico & neumático (Ciclo3) y se presenta en los Festivales Internacionales de Poesía de Córdoba y Rosario, Festival Latinoamericano de Poesía en el Centro (Buenos Aires), Encuentro de la Palabra y en el Festival Internacional de Literatura de Buenos Aires (FILBA).
+
+Como historiador tiene editados dos ensayos: Barbarie y Civilización. Sangre, monstruos y vampiros durante el segundo gobierno de Rosas (2008), por el que recibió la Mención Honorífica del Fondo Nacional de las Artes, Degenerados, anormales y delincuentes. Gestos entre ciencia, política y representaciones en el caso argentino” (2010) y el inclasificable 200 años de monstruos y maravillas argentinas (Beatriz Viterbo Editora, 2015) con prólogo y selección de textos de Gabo, ilustraciones de Christian Montenegro y diseño de Laura Varsky.
+
+Como intérprete, Gabo participó junto a Haydée Schvartz en la puesta de Four Walls de John Cage para el Centro de Experimentación del Teatro Colón de Buenos Aires (temporadas 2009 y 2011), del Festival John Cage y el ciclo “Las músicas dentro de la música” con obras de Berio, Dowland y Gandini, entre otros. En 2014, fue protagonista de la ópera Ese grito es todavía un grito de amor sobre textos de Roland Barthes con Música y Libreto de Gabriel Valverde, Dirección Musical de Juan Carlos Tolosa y Regie de Rubén Szuchmacher. En 2015 estrenó junto a Emilio García Wehbi ARTAUD: lengua ∞ madre en BP.15 Bienal de Performance, sobre el universo y la obra de Antonin Artaud y Ediciones Documenta realizó una cuidada edición del texto de esta performance. También en ese año Gabo presentó Espacio contra el estallado en el Teatro Payró, un recorrido por su discografía con puesta en escena de Rubén Szuchmacher. En 2016 Gabo estrenó dos obras performáticas, Diabólico. La partitura y el mapa compuesta junto con el coreógrafo y bailarín Pablo Lugones y Derivas de La Tempestad que se presentó en Kermesse Shakespeare 2016. En 2017 protagonizó la ópera contemporánea “El astrólogo” con música y libreto de Abel Gilbert y dirección de Walter Jakob.
+
+Además, varios de sus poemas, canciones y cuentos fueron material de trabajo y composición para relatos coreográficos, piezas escénico-vocales, documentales y films argentinos e internacionales.
+
+DISCOS
+Solistas
+– Canciones que un hombre no debería cantar. 2005
+– Todo lo sólido se desvanece en el aire. 2006
+– Mañana no debe seguir siendo esto. 2007
+– Amar temer partir. 2008
+– Boca arriba. 2009
+– La aguja tras la máscara. 2011
+– La primera noche del fantasma. 2013
+– El lapsus del jinete ciego. 2016`;
+
+
+export default [
+  {
+    username,
+    title: titleJazz,
+    date: '2018-09-11T21:00:00',
+    file: null,
+    description: descriptionJazz,
+    latitude: '-34.72292999999996',
+    longitude: '58.25539999999995',
+    address: 'Blend Irish Pub',
+  },
+  {
+    username,
+    title: titleDiaphragm,
+    date: '2018-09-06T21:00:00',
+    file: null,
+    description: descriptionDiaphragm,
+    latitude: '-34.710026175026414',
+    longitude: '-58.280631524753936',
+    address: 'Biergarten Bernal',
+  },
+  {
+    username,
+    title: titleAndamio,
+    date: '2018-08-25T21:00:00',
+    file: null,
+    description: descriptionAndamio,
+    latitude: '-34.71472127947847',
+    longitude: '-58.26667829770256',
+    address: 'El Andamio Espacio Cultural',
+  },
+  {
+    username,
+    title: titleUNQuilombo,
+    date: '2018-09-22T21:00:00',
+    file: null,
+    description: descriptionUNQuilombo,
+    latitude: '-34.70640999999995',
+    longitude: '-58.27717999999999',
+    address: 'Universidad Nacional de Quilmes',
+  },
+  {
+    username,
+    title: titleViernesDeSamba,
+    date: '2018-09-21T:21:00:00',
+    file: null,
+    description: descriptionViernesDeSamba,
+    latitude: '-34.63122999999996',
+    longitude: '-58.49267999999995',
+    address: 'Chau Che Clú',
+  },
+  {
+    username,
+    title: titleAuditorioKraft,
+    date: '2018-09-24T21:00:00',
+    file: null,
+    description: descriptionAuditorioKraft,
+    latitude: '-34.600103245628716',
+    longitude: '-58.37534919830917',
+    address: 'Auditorio Kraft',
+  },
+  {
+    username,
+    title: titleInterlunio,
+    date: '2018-09-22T21:00:00',
+    file: null,
+    description: descriptionInterlunio,
+    latitude: '-34.73251028849926',
+    longitude: '-58.24112757885959',
+    address: 'Interlunio Club Cultural',
+  },
+]
---
+++ app/scripts/components/events/biggerFakeData.js
@@ -0,0 +1,83 @@
+const username = 'santiago';
+
+
+const titleJazz = 'Martes de Jazz y Música Popular ✰ Oscar Giunta Supertrío!';
+const descriptionJazz = `· EDICIÓN DELUXE DEL MARTES DE JAZZ Y MÚSICA POPULAR, Ciclo producido por Tempuja / Sello de Arte todos los Martes en Blend Pub.
+
+Presentamos mega felices a
+► Oscar Giunta SUPERTRÍO!
+Oscar Giunta en batería, composición, arreglos y dirección musical, Hernan Jacinto en piano y Flavio Romero en contrabajo.
+Ya nos visitaron este año, estuvo INCREÍBLE! Así que volvemos a repetir ♥
+Músicos de primer nivel ♦
+
+Les invitamos a su mesa cerca del espectáculo llamando a Blend Pub al : 4253 6058 de 18 a 20 Hs. Con reserva de mesa se cobrará un derecho de show de $100 por persona sobre el valor de lo consumido. Ingresando sin reserva se cobrará en puerta $200 y contarán con una consumición.
+
+:::::::::::::::::::::::::50% OFF en TODAS las Cervezas Artesanales Tiradas (18hs a 21hs):::::::::::::::::::::::
+
+Lxs esperamos! ㋡
+
+Musicalizador oficial de cada Martes: Marcelo Calona
+
+Produce: Tempuja / Sello de Arte 〄 ⓤⓝ ⓟⓡⓞⓓⓤⓒⓣⓞ ⓓⓔ ⓣⓤ ⓜⓔⓝⓣⓔ`;
+
+
+const titleDiaphragm = 'Ciclo Diaphragm ∆ Vol. XXXII Vulebard + VirtualFrizz ·';
+const descriptionDiaphragm = `¡¡Bienvenidxs Diaphragmers a una nueva edición!!
+¡Después de un breve receso volvemos a pleno y con #buenasnuevas!
+¡Y como estamos de Festejo! ¡Te proponemos este increíble Line Up! 🔥
+△ Bandas en vivo:
+Vulebard (#hardrock integrado por Guido Vulebard en Bajo y voz, Hernan Vicente en Batería, Tomi Olmos en Guitarra, Seba Gimenez en Guitarra y Ale Bougie en Teclado.)
+Virtual Frizz (#PowerTrio integrado por Lulo García en Guitarra y voz, Algo De Luca en Batería y Andrés Díaz Santana en Bajo.)
+△ Expo Collage: Diamonsi (Marita Bossi)
+△ Flash Tattoo: Carolina Lio #bestfriend de la casa
+△ Diaphragm Dj Set!
+
+👉 Y además tenemos Nueva Locación → ¡Agradecemos a Biergarten Bernal por abrirnos sus puertas y te contamos que podes venir a disfrutar de unas excelentes artesanales y más; además del exquisite Menú que tienen para vos!
+
+
+Como siempre todas las regulaciones y sonidos a cargo del Diaphragm team (John Braian Lora, Romi Mario y Marcelo Ramirez)
+Organizan: Ciclo Diaphragm y
+Tempuja / Sello de Arte ⓤⓝ ⓟⓡⓞⓓⓤⓒⓣⓞ ⓓⓔ ⓣⓤ ⓜⓔⓝⓣⓔ
+
+Diseño Flyer: Pablo Julián (Chamax)
+Ph: Ezequiel Iglesias`
+
+
+const titleAndamio = 'El pintor a la luna y Les Minots en el Andamio - Quilmes';
+const descriptionAndamio = `Ciertas constelaciones se pusieron de acuerdo para que este encuentro suceda en la bella y agitada ciudad de Quilmes, el 25 de Agosto. Todos al El Andamio Espacio Cultural a volar con El pintor a la luna y Les Minots.
+Hipolito Yrigoyen 58 - Quilmes - 21hs
+Entrada al sobre.`;
+
+
+export default [
+  {
+    username,
+    title: titleJazz,
+    date: '2018-09-11T21:00:00',
+    file: null,
+    description: descriptionJazz,
+    latitude: '-34.72292999999996',
+    longitude: '58.25539999999995',
+    address: 'Blend Irish Pub',
+  },
+  {
+    username,
+    title: titleDiaphragm,
+    date: '2018-09-06T21:00:00',
+    file: null,
+    description: descriptionDiaphragm,
+    latitude: '-34.710026175026414',
+    longitude: '-58.280631524753936',
+    address: 'Biergarten Bernal',
+  },
+  {
+    username,
+    title: titleAndamio,
+    date: '2018-08-25T21:00:00',
+    file: null,
+    description: descriptionAndamio,
+    latitude: '-34.71472127947847',
+    longitude: '-58.26667829770256',
+    address: 'El Andamio Espacio Cultural',
+  },
+]
