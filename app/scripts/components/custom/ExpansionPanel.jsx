0700e078300252402e1b55eda79ad7abbab08ad6
Santiago <sgobotta@gmail.com>
2018-10-06 10:44:04 -0300

    Expansion Panel: adds a default expansion Icon

--- app/scripts/components/custom/ExpansionPanel.jsx
+++ app/scripts/components/custom/ExpansionPanel.jsx
@@ -6,11 +6,12 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 
 export default class CustomExpansionPanel extends React.Component {
   renderSummary() {
-    const { expandIcon, summaryContent, summaryClassName } = this.props;
+    const { expandIcon = <ExpandMoreIcon />, summaryContent, summaryClassName } = this.props;
     return (
       <ExpansionPanelSummary
         expandIcon={expandIcon}
