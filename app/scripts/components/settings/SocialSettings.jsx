5cb99cf9ac806bfbcc55f07656bc2aebc27d93c2
Santiago <sgobotta@gmail.com>
2018-10-06 10:45:28 -0300

    Social Settings - Event List: added an expansion panel for usability

--- app/scripts/components/settings/SocialSettings.jsx
+++ app/scripts/components/settings/SocialSettings.jsx
@@ -1,10 +1,11 @@
 /** React Imports */
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 /** Redux Imports */
 import { connect } from 'react-redux';
 /** Material UI Imports */
-import { withStyles } from '@material-ui/core/styles';
+import { createMuiTheme, withStyles, MuiThemeProvider } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -19,10 +20,31 @@ import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import CheckCircleOutlinedIcon from '@material-ui/icons/CheckCircleOutlined';
 import EventIcon from '@material-ui/icons/Event';
 /** Custom Imports */
+import ExpansionPanel from 'components/custom/ExpansionPanel';
 import { DateUtils } from 'utils/dates';
 import SocialAuth from 'components/authentication/SocialAuth';
 import { importEvents, logoutFb } from 'actions';
 
+
+const expansionPanelTheme = createMuiTheme({
+  overrides: {
+    MuiCardContent: {
+      root: {
+        padding: '0px',
+        background: 'transparent',
+      },
+    },
+    MuiExpansionPanel: {
+      root: {
+        backgroundColor: 'rgb(205, 205, 231)',
+        borderRadius: '0px',
+      },
+      expanded: {
+        backgroundColor: 'rgb(180, 205, 231)',
+      },
+    },
+  },
+});
 
 const styles = theme => ({
   root: {
@@ -72,10 +94,45 @@ const styles = theme => ({
       color: 'rgb(0, 120, 215)',
     },
   },
+  expansionPanel: {
+    padding: '5px 5px 5px 5px',
+    borderRadius: '0px',
+    '&:hover': {
+      backgroundColor: 'rgb(180, 205, 231)',
+      marginBottom: '12px',
+      borderBottomLeftRadius: '7px',
+    },
+    borderTopLeftRadius: '0px !important',
+    borderTopRightRadius: '0px !important',
+    borderBottomRightRadius: '0px !important',
+  },
+  expansionPanelSummary: {
+    padding: '0 4px 0 4px',
+  },
+  expansionPanelDetails: {
+    alignItems: 'center',
+    padding: '0 4px 0 4px',
+  },
+  expansionPanelActions: {
+    padding: '5px 5px 5px 10px',
+  },
 });
 
 
 class SocialSettings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+    };
+  }
+  
+  handlePanelToggling = panel => (event, expanded) => {
+    this.setState({
+      expanded: expanded ? panel : false,
+    });
+  };
+  
   handleStopSocialSync = () => {
     const { finishSocialSync, user } = this.props;
     finishSocialSync({ id: user.data.id });
@@ -150,53 +207,92 @@ class SocialSettings extends React.Component {
     return DateUtils.getParsedDate(date);
   }
 
-  renderEvent(event) {
+  renderExpansionPanelSummary(event) {
     const { classes } = this.props;
     return (
-      <Grid container direction="column" className={classes.events.root}>
-        <Card>
-          <CardContent>
-            <Grid item xs={12}>
-              <Typography align="left" variant="subheading" className={classes.events.date}>
-                {this.renderDate(event.start_time)}
-              </Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography align="center" variant="headline" className={classes.events.title}>
-                {event.name}
-              </Typography>
-            </Grid>
-            <Grid item xs={12} className={classes.events.description}>
-              <TextField
-                fullWidth
-                align="right"
-                InputLabelProps={{ shrink: true }}
-                multiline
-                variant="outlined"
-                margin="normal"
-                value={event.description}
-              >
-                {}
-              </TextField>
-            </Grid>
-          </CardContent>
-          <Divider />
-          <CardActions>
-            <Grid item xs={12}>
-              <Typography align="right" variant="button" className={classes.events.address}>
-                Lugar | {event.place.name}
-              </Typography>
-            </Grid>
-          </CardActions>
-        </Card>
+      <React.Fragment>
+        <Grid item xs={12}>
+          <Typography align="left" variant="subheading" className={classes.events.date}>
+            {this.renderDate(event.start_time)}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography align="center" variant="headline" className={classes.events.title}>
+            {event.name}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography align="right" variant="button" className={classes.events.address}>
+            Lugar | {event.place.name}
+          </Typography>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+
+  renderExpansionPanelDetails(event) {
+    const { classes } = this.props;
+    return (
+      <Grid item xs={12} className={classes.events.description}>
+        <TextField
+          fullWidth
+          align="right"
+          InputLabelProps={{ shrink: true }}
+          multiline
+          variant="outlined"
+          margin="normal"
+          value={event.description}
+        >
+          {}
+        </TextField>
       </Grid>
+    )
+  }
+  
+  renderExpansionPanelActions(event) {
+    const { classes } = this.props;
+    return (
+      <React.Fragment>
+        <Divider />
+        <CardActions>
+          {}
+        </CardActions>
+      </React.Fragment>
+    );
+  }
+
+  renderEvent(event, index) {
+    const { classes } = this.props;
+    const { expanded } = this.state;
+    return (
+      <div key={index}>
+        <Grid container direction="column" className={classes.events.root}>
+          <MuiThemeProvider theme={expansionPanelTheme}>
+            <ExpansionPanel
+              actionsClassName={classes.expansionPanelActions}
+              actionsContent={this.renderExpansionPanelActions(event)}
+              detailsClassName={classes.expansionPanelDetails}
+              detailsContent={this.renderExpansionPanelDetails(event)}
+              expanded={expanded === `panel${index}`}
+              onChange={this.handlePanelToggling(`panel${index}`)}
+              panelClassName={
+                classNames(
+                  classes.expansionPanel
+                )
+              }
+              summaryClassName={classNames(classes.expansionPanelSummary)}
+              summaryContent={this.renderExpansionPanelSummary(event)}
+              />
+          </MuiThemeProvider>
+          </Grid>
+      </div>
     );
   }
 
   renderEvents() {
     const { fbEvents } = this.props;
     if (fbEvents && fbEvents.eventsData && fbEvents.eventsData.data && fbEvents.eventsData.data.length > 0) {
-      const events = fbEvents.eventsData.data.map((event) => this.renderEvent(event));
+      const events = fbEvents.eventsData.data.map((event, index) => this.renderEvent(event, index));
       return events;
     }
     return null;
